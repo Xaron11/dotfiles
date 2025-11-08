4e163fea5dc8df7bccccf71c9664daaa01d1cdc1
Xaron11 <kaczmarzykdawid4@gmail.com>
2025-11-08 10:02:17 +0100

    Enhance, customize and refactor rofi menus: powermenu, power profiles, appmenu, windowmenu, keyhintmenu

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -15,7 +15,10 @@ lock=" Lock"
 logout="󰍃 Logout"
 suspend="󰒲 Sleep"
 shutdown="⏻ Shutdown"
-reboot="󰜉 Restart"
+reboot=" Restart"
+reboot_to_windows=" Reboot to Windows"
+hibernate=" Hibernate"
+cancel=" Cancel"
 
 # Confirmation
 confirm_exit() {
@@ -28,34 +31,51 @@ msg() {
   rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
 }
 
+# Check whether a command exists
+function command_exists() {
+  command -v "$1" &>/dev/null 2>&1
+}
+
+# systemctl required
+if ! command_exists systemctl; then
+  exit 1
+fi
+
 # Variable passed to rofi
-options="$lock\n$logout\n$suspend\n$shutdown\n$reboot"
+options="$lock\n$logout\n$suspend\n$shutdown\n$reboot\n$reboot_to_windows\n$hibernate\n$cancel"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 case $chosen in
-  $shutdown)
-    systemctl poweroff
-    ;;
-  $reboot)
-    systemctl reboot
-    ;;
-  $lock)
-    betterlockscreen -l
-    ;;
-  $suspend)
-    mpc -q pause
-    amixer set Master mute
-    systemctl suspend
-    ;;
-  $logout)
-    if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-      openbox --exit
-    elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-      bspc quit
-    elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-      i3-msg exit
-    elif [[ "$DESKTOP_SESSION" == "awesome" ]]; then
-      awesome-client 'awesome.quit()'
-    fi
-    ;;
+$lock)
+  betterlockscreen -l
+  ;;
+$logout)
+  if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+    openbox --exit
+  elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+    bspc quit
+  elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+    i3-msg exit
+  elif [[ "$DESKTOP_SESSION" == "awesome" ]]; then
+    awesome-client 'awesome.quit()'
+  fi
+  ;;
+$suspend)
+  mpc -q pause
+  amixer set Master mute
+  systemctl suspend
+  ;;
+$shutdown)
+  systemctl poweroff
+  ;;
+$reboot)
+  systemctl reboot
+  ;;
+$hibernate)
+  systemctl hibernate
+  ;;
+$reboot_to_windows)
+  systemctl start reboot-to-windows.service
+  ;;
+$cancel) ;;
 esac
