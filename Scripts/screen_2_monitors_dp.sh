2d895283cb3bcc1294d36455b89a6dd037d21f76
Xaron11 <me@xaron11.dev>
2026-03-05 18:22:36 +0100

    Add xrandr screen layout scripts

---
+++ Scripts/screen_2_monitors_dp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1920x1080 --rate 120 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output DP-1-0 --primary --mode 1920x1080 --rate 120 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-1-0 --off
---
+++ Scripts/screen_aoc_hdmi.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-1-0 --auto --rate 60.00 --size 1920x1080 --right-of eDP1 --output eDP1 --auto --rate 120.00 --size 1920x1080
